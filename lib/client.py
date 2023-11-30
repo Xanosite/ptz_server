c4b7677bea2a31f263405516da2efff87f1105e6
Blake <bh@pautzke.com>
2023-11-30 23:07:35 +0000

    Redid client connections manager to use asyncio

--- lib/client.py
+++ lib/client.py
@@ -1,9 +1,9 @@
+import ast
 import asyncio
 import logging
-import socket
 
 MAGIC = 'pr7d68j1'
-S_PORT = 50201
+M_PORT = int(50201)
 
 class Handler:
     """
@@ -17,145 +17,94 @@ class Handler:
     status_mon = {}
     clients = {}
 
-    def __init__(self, host: str) -> None:
+    def __init__(self, host: str, port: int=M_PORT) -> None:
         """
         host: ipv4 address of host system
         """
-        self.active = True
         self.clients = []
         self.host = host
-        self.hostname = socket.getfqdn()
-        self.port = None
-        self.serversock = None
-
-    async def announce_self(self) -> None:
-        """
-        Broadcast actual server port via UDP
-        """
-        logging.info('Server port announcement service stared')
-        udp_serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_serversocket.bind(self.host, S_PORT)
-        udp_serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        data = (MAGIC, self.hostname, self.port)
-        while self.active:
-            self.status_mon['announce_self'] = 'Active'
-            await send_udp(
-                udp_serversocket, (MAGIC, self.hostname, self.port), S_PORT
-            )
-            logging.debug(
-                f'Service port announcement broadcasted from {self.hostname}:'
-                f'{S_PORT}\n    Data: {data}'
-            )
-            await asyncio.sleep(5)
-        udp_serversocket.shutdown(socket.SHUT_RDWR)
-        udp_serversocket.close()
-        self.status_mon['announce_self'] = 'Inactive'
-        logging.info('Server port announcement service stopped')
+        self.port = port
+        self.serv = None
 
     async def listen_clients(self) -> None:
         """
         Listens for connections, spawns new clients on connect
         """
+        async def new_client(reader: asyncio.StreamReader,
+                             writer: asyncio.StreamWriter) -> None:
+            """
+            Manage new client connections
+            """
+            client = Client(reader, writer, self)
+            self.clients.append(client)
+            await client.start()
         # Start up server socket for listening
-        self.serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serversock.bind((self.host, 0))
-        self.port = self.serversock.getsockname()[1]
-        self.serversock.listen(10)
-        logging.info(f'Listening on {self.host}:{self.port}')
-        # Listen for new connections, spawn new clients
-        while self.active:
-            self.status_mon['listen_clients'] = 'Active'
-            connection, address = self.serversock.accept()
-            logging.debug(f'Accepted connection to {address[0]}')
-            self.clients.append(Client(connection, address[0]))
-            await asyncio.sleep()
-        # Shut down listening socket if handler becomes inactive
-        self.status_mon['listen_clients'] = 'Inactive'
-        self.serversock.shutdown(socket.SHUT_RDWR)
-        self.serversock.close()
-        self.serversock = None
-        logging.info('Listening service stopped')
-
-    def close_client(self, client) -> None:
-        """
-        Close client and remove from tracked clients
-        client: client to be closed
-        """
-        client.close()
-        del self.clients[client.get_address()]
-
+        self.serv = await asyncio.start_server(new_client, self.host, self.port)
+        logging.info(
+            f'Client listening server started at {self.host}:{self.port}'
+        )
+        
     async def close(self) -> None:
         """
         Graceful shutdown of client manager
         """
-        self.active = False
-        for client in self.clients:
-            client.close()
-        timer = 0
-        waiting = True
-        while waiting:
-            if 'Active' in self.status_mon.values(): waiting = True
-            elif len(self.clients) > 0: waiting = True
-            else: waiting = False
-            if timer > 9:
-                logging.warning('Client handler did not shut down properly')
-                waiting = False
-            if waiting:
-                display = '  Status monitor:\n'
-                for activity in self.status_mon:
-                    display += f'    {activity}:{self.status_mon[activity]}\n'
-                display += '  Clients:\n'
-                for client in self.clients:
-                    logging.debug(f'{len(self.clients)} clients still connected')
-                logging.debug(f'Client handler waiting:\n{display}')
-                await asyncio.sleep(2)
-                timer += 2
-        logging.info('Client handler service stopped')
+        logging.info("Client connection server stop command received")
+        logging.debug(f'Clients connected: {len(self.clients)}')
+        if len(self.clients) > 0:
+            for client in self.clients:
+                await client.close()
+        logging.debug(f'Clients connected: {len(self.clients)}')
+        self.serv.close()
+        await self.serv.wait_closed()
+        logging.info("Client connection server stopped")
 
 class Client:
     """
     Client manager
     """
-    address = None
-    connection = None
-    def __init__(self, connection: socket.socket, address: str) -> None:
-        self.address = address
-        self.connection = connection
+    def __init__(self, reader: asyncio.StreamReader,
+                 writer: asyncio.StreamWriter,
+                 handler: Handler) -> None:
+        self.reader = reader
+        self.writer = writer
+        self.status = {}
+        self.handler = handler
 
-    def get_address(self) -> str:
-        return self.address
+    async def start(self) -> None:
+        await self.send_to_client(self.status)
 
-    def close(self) -> None:
-        self.connection.shutdown(socket.SHUT_RDWR)
-        self.connection.close()
-        logging.debug(f'Connection closed to {self.address}')
+    async def close(self) -> None:
+        if self.writer.is_closing():
+            return None
+        else:
+            self.writer.close()
+            await self.writer.wait_closed()
 
-async def send_to_sock(socket: socket.socket, data: dict) -> None:
-    """
-    Send data to active socket
-    Data sent as bytes string of dictionary
-    """
-    b_data = bytes(str(data), 'utf-8')
-    total_sent = len(b_data)
-    sent = 0
-    while sent < total_sent:
-        sent = socket.send(b_data[total_sent:])
-        if sent == 0:
-            raise RuntimeError('Socket connection broken')
-        total_sent += sent
-        if sent < total_sent: await asyncio.sleep()
+    async def send_to_client(self, data: dict) -> None:
+        b_data = bytes(str(data), 'utf-8')
+        self.writer.write(b_data)
+        await self.writer.drain()
 
-async def send_udp(socket: socket.socket, data: dict, port: int) -> None:
-    """
-    Broadcast data via UDP socket
-    Data sent as bytes string of dictionary
-    """
-    b_data = bytes(str(data), 'utf-8')
-    total_sent = len(b_data)
-    sent = 0
-    while sent < total_sent:
-        sent = socket.sendto(b_data[total_sent:], ('<broadcast>', port))
-        if sent == 0:
-            raise RuntimeError('Socket error')
-        total_sent += sent
-        if sent < total_sent: await asyncio.sleep()
+    async def receive_from_client(self) -> dict:
+        data = b''
+        while True:
+            chunk = await self.reader.read(100)
+            if chunk == b'': break
+            data += chunk
+        return ast.literal_eval(data.decode('utf-8'))
+        
+    async def parse_client_data(self, data: dict) -> None:
+        # Commands
+        if 'command' in data.keys():
+            match data['command']:
+                case 'dev_kit': await self.cmd_dev_kit(data)
+
+    async def com_shutdown(self, target: str) -> None:
+        match target:
+            case 'handler': self.handler.close()
+            case 'client': self.close()
+    
+    async def cmd_dev_kit(self, data: dict) -> None:
+        match data['command']:
+            case 'disconnect_client': self.close()
+            case 'stop_client_server': self.handler.close()
--- unit_tests.py
+++ unit_tests.py
@@ -1,26 +1,79 @@
 import asyncio
-#import client_lib
+import logging
+import pathlib
+import lib.client as client_lib
+from datetime import datetime
 
 async def main() -> None:
-    await run_test('Test Tester', test_tester)
+    mdir = pathlib.Path(__file__).parent.resolve()
+    init_logger(mdir)
+    logging.info('#### Unit tests started. ####')
+    print(('#'*25) +'  Pautzke Server: Unit tests  ' + ('#'*25))
+    await test_tester()
+    await test_handler()
 
 def make_test_display(test_name: str, status: int) -> str:
     match status:
         case 0: state = 'Running'
         case 1: state = 'Passed'
-        case 2: pass
-        case 3: pass
+        case 2: state = 'Failed'
+        case 3: state = 'Unknown'
         case 4: pass
         case 5: pass
     space = 80 - len(test_name) - len(state)
     return test_name + (' . ' * int(space/3)) + (' ' * (space % 3)) + state
 
-async def run_test(test_name, test) -> None:
-    print(make_test_display(test_name,0), end='\r')
-    print(make_test_display(test_name, await test()))
+def print_test(test_name, status: int, end: bool) -> None:
+    if end: print(make_test_display(test_name, status))
+    else: print(make_test_display(test_name,0), end='\r')
+    
 
-async def test_tester() -> int:
-    return 1
+def init_logger(mdir):
+    fname = datetime.now().strftime('%Y-%m-%d') + '.log'
+    logging.basicConfig(
+        filename = pathlib.Path(mdir/'logs'/fname),
+        level = logging.DEBUG,
+        format = '%(levelname)s %(asctime)s %(message)s',
+        datefmt = '%H:%M:%S'
+    )
+
+# Tests
+async def test_tester():
+    print_test('Test Tester', 0, False)
+    print_test('Test Tester', 1, True)
+
+async def test_handler():
+    status = 0
+    print_test('Client Server', status, False)
+    handler = None
+    try:
+        handler = client_lib.Handler('172.233.157.205')
+        await handler.listen_clients()
+    except Exception as err:
+        logging.error(err)
+        status = 2
+    else: status = 1
+    print_test('Client Server', status, True)
+    status = 3 if status == 2 else 0
+    print_test('Client connection', status, status == 3)
+    if status == 3: return None
+    try:
+        reader, writer = await asyncio.open_connection('172.233.157.205', 50201)
+        data = await reader.read(1000)
+        data = data.decode('utf-8')
+        writer.close()
+        await writer.wait_closed()
+        reader2, writer2 = await asyncio.open_connection('172.233.157.205', 50201)
+        data2 = await reader2.read(1000)
+        writer2.close()
+        await writer2.wait_closed()
+        await handler.close()
+    except Exception as err:
+        logging.error(err)
+        status = 2
+    else: status = 1
+    print_test('Client Connection', status, True)
 
 if __name__ == '__main__':
     asyncio.run(main())
+
